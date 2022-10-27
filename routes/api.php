15e43f6d307a96f7a589199ad8c53be208c660cf
Raihan <3103120183@student.smktelkom-pwt.sch.id>
2022-10-27 13:39:59 +0700

    Challenge-Author

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,8 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('me',[AuthController::class, 'me']);
+Route::get('me', [AuthController::class, 'me']);
 
 Route::resource('books', BookController::class)->except(
+    ['create', 'edit']
+);
+
+Route::resource('authors', AuthorController::class)->except(
     ['create', 'edit']
 );
